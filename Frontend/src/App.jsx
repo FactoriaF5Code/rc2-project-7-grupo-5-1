dcad4c6bc5430c37edb554d0e588cd76ba5db84f
herminiocastro <hxssport@gmail.com>
2024-01-25 13:07:19 +0100

    pagina2 empezada

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
-import Header from "./Header/Header"
+import Pagina2 from "./Pagina2/Pagina2.jsx"
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css'
+import Header from './Header/Header.jsx'  
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,8 +10,15 @@ function App() {
   return (
     <>
      <Header />
+     <Pagina2 />
     </>
   )
 }
+
+const App = () => (
+  <Router>
+    <Route path="/pagina2" component={Pagina2} />
+  </Router>
+);
 
 export default App
