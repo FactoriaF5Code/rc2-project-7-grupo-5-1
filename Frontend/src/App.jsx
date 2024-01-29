127e58ddc0bf49810bdfaa01934cf5b0973dffe2
enolsurfer <39172327+enolsurfer@users.noreply.github.com>
2024-01-29 08:59:03 +0100

    cambios modal

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import ButtonAñadir from "./Buttons/ButtonAñadir.jsx";
+import AddButton from "./Buttons/ButtonAñadir.jsx";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
@@ -8,13 +8,23 @@ import MyModal from "./Modal/Modal";
 import Header from "./Header/Header.jsx";
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
 
   return (
     <>
       <Router>
         <Header />
-        <ButtonAñadir />
-        <MyModal />
+        <AddButton onOpenModal={handleOpenModal} />
+        <MyModal isOpen={isModalOpen} onCloseModal={handleCloseModal} />
       </Router>
     </>
   );
