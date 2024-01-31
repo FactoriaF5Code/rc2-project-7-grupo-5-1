487b66d9d9979ef17d3b1c07d5da0b7f9b7fd5ce
enolsurfer <39172327+enolsurfer@users.noreply.github.com>
2024-01-31 12:55:14 +0100

    boton eliminar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import AddButton from "./Buttons/ButtonAñadir.jsx";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
@@ -15,7 +15,7 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:8080/urls');
+        const response = await axios.get("http://localhost:8080/urls");
         setData(response.data);
       } catch (error) {
         console.error(error);
@@ -33,7 +33,8 @@ function App() {
     setIsModalOpen(false);
   };
 
-  const handleSubmit = (event) => { // Nueva función para manejar el envío del formulario
+  const handleSubmit = (event) => {
+    // Nueva función para manejar el envío del formulario
     event.preventDefault();
 
     if (!title.trim() || !url.trim()) {
@@ -42,31 +43,31 @@ function App() {
     }
 
     const handleSubmit = async (event) => {
-  event.preventDefault();
+      event.preventDefault();
 
-  if (!title.trim() || !url.trim()) {
-    alert("Ambos campos son obligatorios");
-    return;
-  }
+      if (!title.trim() || !url.trim()) {
+        alert("Ambos campos son obligatorios");
+        return;
+      }
 
-  try {
-    const response = await axios.post('http://localhost:8080/urls', {
-      title: title,
-      url: url
-    });
+      try {
+        const response = await axios.post("http://localhost:8080/urls", {
+          title: title,
+          url: url,
+        });
 
-    if (response.status === 200) {
-      alert('Datos enviados con éxito');
-      setTitle('');
-      setUrl('');
-    } else {
-      alert('Hubo un error al enviar los datos');
-    }
-  } catch (error) {
-    console.error(error);
-    alert('Hubo un error al enviar los datos');
-  }
-};
+        if (response.status === 200) {
+          alert("Datos enviados con éxito");
+          setTitle("");
+          setUrl("");
+        } else {
+          alert("Hubo un error al enviar los datos");
+        }
+      } catch (error) {
+        console.error(error);
+        alert("Hubo un error al enviar los datos");
+      }
+    };
   };
 
   const handleDelete = async (id) => {
@@ -90,31 +91,35 @@ function App() {
         <AddButton onOpenModal={handleOpenModal} />
         <MyModal isOpen={isModalOpen} onCloseModal={handleCloseModal} />
         {data.map((item, index) => (
-  <div className="barButtom" key={index}>
-    <h2>{item.title}</h2>
-    <p>
-      <a href={item.url} target="_blank" rel="noopener noreferrer">
-        {item.url}
-      </a>
-    </p>
-    <button
-      className="noselect"
-      onClick={() => handleDelete(item.id)}
-    >
-      <span className="text">Eliminar</span>
-      <span className="icon">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-        >
-          <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path>
-        </svg>
-      </span>
-    </button>
-  </div>
-))}
+          <div className="barButtom" key={index}>
+            <div className="url-container">
+              <h2>{item.title}</h2>
+              <p>
+                <a href={item.url} target="_blank" rel="noopener noreferrer">
+                  {item.url}
+                </a>
+              </p>
+            </div>
+            <div className="buton-delete">
+              <button
+                className="noselect"
+                onClick={() => handleDelete(item.id)}
+              >
+                <span className="text">Eliminar</span>
+                <span className="icon">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path>
+                  </svg>
+                </span>
+              </button>
+            </div>
+          </div>
+        ))}
       </Router>
     </>
   );
