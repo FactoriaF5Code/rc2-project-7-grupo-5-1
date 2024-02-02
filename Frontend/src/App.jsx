b2fafa9a723d2a04b849d1a1cedc2202d6b145d7
herminiocastro <hxssport@gmail.com>
2024-02-02 09:36:15 +0100

    Refactorizar boton de eliminar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import AddButton from "./Buttons/ButtonAñadir.jsx";
+import DeleteButton from "./Buttons/DeleteButton.jsx";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import MyModal from "./Modal/Modal.jsx";
@@ -100,24 +101,7 @@ function App() {
                 </a>
               </p>
             </div>
-            <div className="buton-delete">
-              <button
-                className="noselect"
-                onClick={() => handleDelete(item.id)}
-              >
-                <span className="text">Eliminar</span>
-                <span className="icon">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path>
-                  </svg>
-                </span>
-              </button>
-            </div>
+            <DeleteButton handleDelete={() => handleDelete(item.id)} />
           </div>
         ))}
       </Router>
