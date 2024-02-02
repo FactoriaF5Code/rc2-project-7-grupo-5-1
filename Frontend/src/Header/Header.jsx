54af21c5759d3aa09aa891cfae771d69eeee041c
herminiocastro <hxssport@gmail.com>
2024-02-02 09:16:52 +0100

    Eliminado burguer menu y search bar

--- Frontend/src/Header/Header.jsx
+++ Frontend/src/Header/Header.jsx
@@ -3,22 +3,10 @@ import Logo from "../asset/INFO CODER LOGO OK.png";
 function Header() {
   return (
     <header>
-      <div className="logo" >
-          <label for="burger" class="burger">
-        <input id="burger" type="checkbox"></input>
-        <span></span>
-        <span></span>
-        <span></span>
-      </label>
-      
-      <img className="imgLogo" src = {Logo} alt="INFO CODER"  />
-      
+      <div className="logo">
+        <img className="imgLogo" src={Logo} alt="INFO CODER" />
       </div>
-            <div className="search-container">
-
-        <input className="search-input" type="search" placeholder="Buscar"></input>
-      </div>
-          </header>
+    </header>
   );
 }
 
