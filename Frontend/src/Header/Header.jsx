b781cd9c908c713cb6910e8a0bbf2ee4a41a491d
Anadevega <soydevega@gmail.com>
2024-02-05 15:06:37 +0100

    lupaok

--- Frontend/src/Header/Header.jsx
+++ Frontend/src/Header/Header.jsx
@@ -3,6 +3,8 @@ import ButtonAñadir from "../Buttons/ButtonAñadir"
 import Logo from "../asset/INFO CODER LOGO OK.png";
 import { useState } from "react";
 import MyModal from "../Modal/Modal.jsx";
+import SearchIcon from "@mui/icons-material/Search";
+
 function Header() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const handleOpenModal = () => {
@@ -11,18 +13,28 @@ function Header() {
   const handleCloseModal = () => {
     setIsModalOpen(false);
   };
+  const handleSearch = () => {
+    // Lógica de búsqueda aquí
+    console.log("Realizar búsqueda");
+  };
   return (
     <header>
-      <div>
-      <div className="logo"/>
+      <div className="logo">
         <img className="imgLogo" src={Logo} alt="INFO CODER" />
-        </div>
-        <div className="ButtonAñadir">
-        <ButtonAñadir onOpenModal={handleOpenModal} className = "ButtonAñadir" />
-        <div/>
-        <MyModal isOpen={isModalOpen} onCloseModal={handleCloseModal} />
       </div>
 
+      <div className="search-container">
+        <input className="search-input" type="search" placeholder="Buscar" />
+        <button className="search-button" onClick={handleSearch}>
+        <SearchIcon style={{ width: "25px", height: "25px" }} />
+
+        </button>
+      </div>
+
+      <div className="ButtonAñadir">
+        <ButtonAñadir onOpenModal={handleOpenModal} />
+        <MyModal isOpen={isModalOpen} onCloseModal={handleCloseModal} />
+      </div>
     </header>
   );
 }
